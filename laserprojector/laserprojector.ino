38875715efe9fa38d338a5a1d9a55466236ce62d
Sterling Schulkins <sterling.schulkins@gmail.com>
2025-03-19 22:22:33 -0700

    adding lcd

--- laserprojector/laserprojector.ino
+++ laserprojector/laserprojector.ino
@@ -27,10 +27,60 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
+#include <LiquidCrystal.h>
 
 #define LASER_CONTROL_PIN 46
 
 static bool laser_state = false;
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
+
+*/
+
+
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 45, en = 42, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 /**********************************************  WIFI Client *********************************
 
@@ -219,12 +269,28 @@ void setupWIFI()
   
 }
 
+void setup_lcd() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(20, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop_lcd() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
+
 /******************************************************
  * arduino setup
  */
 void setup()
 {
   pinMode(LASER_CONTROL_PIN, OUTPUT);
+  setup_lcd();
   display.init();
   display.clear();
   display.display();
@@ -242,6 +308,7 @@ void setup()
 
   setupWIFI();
   setupOTA();
+  
 }
 
 /******************************************************
@@ -262,7 +329,7 @@ void loop()
       digitalWrite(LASER_CONTROL_PIN, LOW); // turn the laser off
     }
   }
-  
+  loop_lcd();
 }
 
 /****************************************************
